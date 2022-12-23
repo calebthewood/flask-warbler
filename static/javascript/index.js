f6bacc60c85fcfde3b5002e371590d875dab3c21
calebthewood <calebwood.cs@gmail.com>
2022-12-22 20:56:26 -0800

    Add listeners for following and follower templates

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,32 +1,64 @@
 "use strict";
 
+console.log("Icons by Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.");
 // Select the div containing message related buttons
-const messageBtns = document.querySelectorAll('.message-btns');
+const messages = document.getElementById('messages');
+const userProfileBtns = document.getElementById('user-profile-btns');
+const followingBtn = document.getElementById('following-btns');
 
-// Handle Clicks on message buttons
-messageBtns.addEventListener('click', async event => {
-  const actions = ["like", "unlike","follow", "unfollow"]
-  const action = event.target.getAttribute('data-message-action')
-  if (actions.includes(action)) {
-    const messageId = event.target.getAttribute('data-message-id');
-    let resource = (action === "like" || action === "unlike") ? "messages" : "users"
+// Handle Clicks on (un)like/(un)follow buttons
+if (messages) messages.addEventListener('click', handleBtns);
+if (userProfileBtns) userProfileBtns.addEventListener('click', handleBtns);
+if (followingBtn) followingBtn.addEventListener('click', handleBtns);
+
+
+async function handleBtns(event) {
+  console.log("Handle Buttons");
+  if (event.target.tagName === 'BUTTON') {
+    const endpoint = event.target.getAttribute('data-endpoint');
+    const { classList } = event.target;
+    // Update Button's classlist
+    if (classList.contains("like")) {
+      event.target.classList.replace("like", "unlike");
+    } else if (classList.contains("unlike")) {
+      event.target.classList.replace("unlike", "like");
+    } else if (classList.contains("follow")) {
+      event.target.classList.replace("follow", "unfollow");
+      event.target.innerHTML = icons.unfollow;
+    } else if (classList.contains("unfollow")) {
+      event.target.classList.replace("unfollow", "follow");
+      event.target.innerHTML = icons.follow;
+    }
+    event.target.setAttribute('data-endpoint', toggleEndpoint(endpoint));
 
     try {
-      const response = await sendAjaxRequest(resource, messageId, action);
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
       console.log("Action Handler Response: ", response);
     } catch (error) {
       console.error(error);
     }
   }
-});
+}
 
-async function sendAjaxRequest(resource,id,action) {
-  // Use the fetch API to send an AJAX request
-  const response = await fetch(`/${resource}/${id}/${action}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  return;
+
+/** Accepts a given endpoint and returns the complementary endpoint.
+ *  Works for likes and follows.
+ *
+ *    Likes: "/messages/42/unlike" >> "/messages/42/like"
+ *
+ *    Follows: "/users/stop-following/13" >> "/users/follow/13"
+ */
+function toggleEndpoint(endpoint) {
+  const params = endpoint.split("/");
+  if (params[1] === "users") {
+    params[2] = params[2] == "follow" ? "stop-following" : "follow";
+  } else {
+    params[3] = params[3] == "like" ? "unlike" : "like";
+  }
+  return params.join("/");
 }
