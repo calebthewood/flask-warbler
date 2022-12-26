1df47fa96b600a840bac4e25f25c09af10ec26e8
calebthewood <calebwood.cs@gmail.com>
2022-12-26 13:44:30 -0800

    Remove unnecessary js modal code

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,14 +1,5 @@
 "use strict";
 
-/**************** Modals */
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-});
-
-
 console.log("Icons by Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.");
 // Select the div containing message related buttons
 const messages = document.getElementById('messages');
